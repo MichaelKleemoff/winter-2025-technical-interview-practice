fccf1d0de895777d7de56f19e7d2b3f95830a239
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-01-26 10:51:52 -0500

    Complete 'lesson02-strings/warmup.js'.

--- lesson02-strings/warmup.js
+++ lesson02-strings/warmup.js
@@ -1,15 +1,25 @@
 // 1. given a string with three characters, return the second character
+function returnChar(str) {
+	return str[1];
+}
+console.log(returnChar('Michael'));
 
 // 2. get the second and third char from a string. the string will always have atleast three characters
+function returnSecondThirdChar(str) {
+	return str.substring(1, 3);
+}
+console.log(returnSecondThirdChar('cat'));
+console.log(returnSecondThirdChar('robbed'));
 
 // 'cat' => 'at'
 // 'robbed' => 'ob'
 
 // 3. Given a word with 5 letters, return the second through 5th letter
 
-// function getSecondThroughFifthLetter (word){
-//   return word.slice(1);
-// }
+function getSecondThroughFifthLetter(word) {
+	return word.slice(1);
+}
+console.log(getSecondThroughFifthLetter('apple'));
 
 // 4. given a string and a number called N, return the second through the Nth character
 
@@ -19,9 +29,19 @@
 // slice is a ?? method: array and also a string method
 // splice is a array method
 
+function getSecondToNth(str, N) {
+	return str.slice(1, N);
+}
+console.log(getSecondToNth('schoolbus', 5));
+console.log(getSecondToNth('telephone', 6));
+
 // 5. Given an array of booleans, return an arrray of only false elements
 
 // [true, true, false, true , false] => [false, false]
+function getFalseElements(arr) {
+	return arr.filter((element) => element === false);
+}
+console.log(getFalseElements([true, true, false, true, false]));
 
 // 6. given a string with two words, put ' the ' between the words and return the string
 
@@ -29,6 +49,15 @@
 // 'for real' => 'for the real'
 // 'jim john' => 'jim the john'
 // 'to house' => 'to the house'
+
+function insertThe(str) {
+	const words = str.split(' '); // Split the string into an array of words
+	return words.join(' the '); // Join the words with ' the ' in between
+}
+console.log(insertThe('to beach'));
+console.log(insertThe('for real'));
+console.log(insertThe('jim john'));
+console.log(insertThe('to house'));
 
 // 7.  given two numbers, price and discount, return the price after the discount is applied
 
@@ -39,3 +68,9 @@
 // 70, 3 => 67.9
 
 // Console.log each of your answers with the test cases provided. Attempt to write each one with TDD.
+
+function getPriceAfterDiscount(price, discount) {
+	return price - price * (discount * 0.01);
+}
+console.log(getPriceAfterDiscount(100, 15));
+console.log(getPriceAfterDiscount(70, 3));
