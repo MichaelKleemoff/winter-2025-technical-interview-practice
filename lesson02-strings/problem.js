30b7e538a0fb3ff64a1cccea8e2ee9c0fc4541f9
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-01-26 11:00:41 -0500

    Complete 'lesson02-strings/problem.js'.

--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -5,7 +5,20 @@
  * @return {number}
  */
 function lengthOfLongestSubstring(s) {
-  // Your code here
+	let charSet = new Set(); // To store unique characters
+	let maxLength = 0; // To keep track of the longest substring
+	let left = 0; // Left pointer of the sliding window
+
+	for (let right = 0; right < s.length; right++) {
+		while (charSet.has(s[right])) {
+			charSet.delete(s[left]); // Remove the leftmost character
+			left++; // Move the left pointer to shrink the window
+		}
+		charSet.add(s[right]); // Add the current character to the set
+		maxLength = Math.max(maxLength, right - left + 1); // Update max length
+	}
+
+	return maxLength;
 }
 
 module.exports = lengthOfLongestSubstring;
