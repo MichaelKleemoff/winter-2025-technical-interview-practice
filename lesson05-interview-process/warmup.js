47c63708c0504988a60dc354dd29f47f21f81af3
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-02-02 16:54:39 -0500

    Complete 'lesson05-interview-process/warmup.js'.

--- lesson05-interview-process/warmup.js
+++ lesson05-interview-process/warmup.js
@@ -1,20 +1,44 @@
-function reverseString(str) {}
+function reverseString(str) {
+	let newStr = '';
+	for (let i = str.length - 1; i >= 0; i--) {
+		newStr += str[i];
+	}
+	return newStr;
+}
 
-console.log(reverseString("Hello, World!"));
+console.log(reverseString('Hello, World!'));
 // Expected Output: "!dlroW ,olleH"
 
-function capitalizeWords(str) {}
+function capitalizeWords(str) {
+	const words = str.split(' ');
+	for (let i = 0; i < words.length; i++) {
+		words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+	}
 
-console.log(capitalizeWords("hello, world!"));
+	return words.join(' ');
+}
+
+console.log(capitalizeWords('hello, world!'));
 // Expected Output: 'Hello, World!'
 
-console.log(capitalizeWords("javascript is awesome"));
+console.log(capitalizeWords('javascript is awesome'));
 // Expected Output: 'Javascript Is Awesome'
 
-function countVowels(str) {}
+function countVowels(str) {
+	const vowels = 'aeiou';
+	let vowelCount = 0;
 
-console.log(countVowels("hello"));
+	for (let i = 0; i < str.length; i++) {
+		if (vowels.includes(str[i])) {
+			vowelCount++;
+		}
+	}
+
+	return vowelCount;
+}
+
+console.log(countVowels('hello'));
 // Expected Output: 2
 
-console.log(countVowels("programming"));
+console.log(countVowels('programming'));
 // Expected Output: 3
