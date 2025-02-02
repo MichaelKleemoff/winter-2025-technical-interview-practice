cbe2d1a590354a7ac2c62719615589c4404c98d6
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-02-02 14:05:54 -0500

    Complete 'lesson06-arrays/problem.js'.

--- lesson06-arrays/problem.js
+++ lesson06-arrays/problem.js
@@ -5,6 +5,20 @@
  * @param {number[]} nums - The integer array.
  * @returns {number} - The index of the largest element if it meets the condition, otherwise -1.
  */
-function checkLargestElement(nums) {}
+function checkLargestElement(nums) {
+	let max = Math.max(...nums);
+	let maxIndex = nums.indexOf(max);
+
+	for (let i = 0; i < nums.length; i++) {
+		if (i !== maxIndex && nums[maxIndex] < 2 * nums[i]) {
+			return -1;
+		}
+	}
+
+	return maxIndex;
+}
+console.log(checkLargestElement([1, 5, 3, 9, 2]));
+console.log(checkLargestElement([-7, -2, -5, -1]));
+console.log(checkLargestElement([-7, 5, -5, 1]));
 
 module.exports = checkLargestElement;
